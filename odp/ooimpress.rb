2f5fed7b815595ef82c29825dbda0e8eeed00c1b
Arnau Sanchez <pyarnau@gmail.com>
2011-05-04 10:43:02 +0200

    ensure cd to old directory

--- odp/ooimpress.rb
+++ odp/ooimpress.rb
@@ -5,7 +5,7 @@ require 'tmpdir'
 require 'erb'
 require 'ostruct'
 
-# Third-party gems: 
+# Third-party gems
 require 'escape' # http://www.a-k-r.org/escape/
 
 class ErbBinding < OpenStruct
@@ -19,22 +19,24 @@ module OOImpress
     path = File.expand_path(output_path)
     FileUtils.rm_f(path)
     old_directory = FileUtils.pwd
-    FileUtils.cd(tree_directory) 
-    run(["zip", "-0", "-X", path, "mimetype"])
-    run(["zip", "-r", path, ".", "-x", "mimetype"])
-    FileUtils.cd(old_directory)
+    FileUtils.cd(tree_directory)
+    begin 
+      run(["zip", "-0", "-X", path, "mimetype"])
+      run(["zip", "-r", path, ".", "-x", "mimetype"])
+    ensure
+      FileUtils.cd(old_directory)
+    end
     path
   end
-  
 
-  def self.create_document_from_template(tree_directory, output_path, namespace)
+  def self.create_document_from_template(tree_directory, output_path, namespaces)
     temporal_directory = Dir.mktmpdir
     begin
       FileUtils.cp_r(Dir.glob(File.join(tree_directory, "*")), temporal_directory)      
-      namespace.each do |template_path, ns|
+      namespaces.each do |template_path, namespace|
         template = ERB.new(File.read(File.join(tree_directory, template_path)))
         open(File.join(temporal_directory, template_path), "w") do |file|
-          file.write(template.result(ErbBinding.new(ns).get_binding))
+          file.write(template.result(ErbBinding.new(namespace).get_binding))
         end
       end
       OOImpress.create_document_from_tree(temporal_directory, output_path)
@@ -49,19 +51,19 @@ private
     command = Escape.shell_command(command_array)
     output = %x{#{command}}
     status = $?.exitstatus
-    fail "Exit status #{status} for command: #{command}" unless status == 0
+    fail "Error: exit status #{status}: #{command}" unless status == 0
     output
   end  
 end
 
 if __FILE__ == $0
   tree_directory, output_path = ARGV
-  namespace = {
+  namespaces = {
     "content.xml" => {
       :company => "IbcMass",
       :date_start => "10/1/2010",
       :date_end => "10/3/2011",
     }
   }
-  puts OOImpress.create_document_from_template(tree_directory, output_path, namespace)
+  puts OOImpress.create_document_from_template(tree_directory, output_path, namespaces)
 end
